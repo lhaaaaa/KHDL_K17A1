85ff22659bfff6f922418b15dcb1e462f9dc9fe5
lhaaaaa <lhanh@uneti.edu.vn>
2023-11-12 23:58:35 +0700

    Đáp án tham khảo

---
+++ Chapter 5/5.2.py
@@ -0,0 +1,6 @@
+x = 10 
+y = 5 
+print("Tổng của", x, "và", y, "là:", x + y)
+print("Hiệu của", x, "và", y, "là:", x - y)
+print("Tích của", x, "và", y, "là:", x*y)
+print("Thương của", x, "cho", y, "là:", x/y)
---
+++ Chapter 5/5.3.py
@@ -0,0 +1,15 @@
+#In ra màn hình theo yêu cầu đề bài
+print("Tên hàng: Sữa hộp Vinamilk")
+print("Số lượng: 5")
+print("Đơn giá: 25000")
+print("Tiền phải trả: 125,000 vnd")
+
+#Thay đổi các giá trị và in ra màn hình
+ten_hang = input("Tên hàng:")
+so_luong = int(input("Số lượng:"))
+don_gia = int(input("Đơn giá:"))
+thanh_tien = so_luong * don_gia
+print("Tên hàng: ", ten_hang, "\n"
+      "Số lượng: ", so_luong, "\n"
+      "Đơn giá: ", don_gia, "\n"
+      "Tiền phải trả: ", thanh_tien)
